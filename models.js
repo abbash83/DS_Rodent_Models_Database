faeb7a7e822660d400b8d4e7234bfac91741b5cc
razam7 <razam7@udayton.edu>
2026-03-27 14:19:07 -0400

    Initial database setup

--- models.js
+++ models.js
@@ -1,94 +1,362 @@
 const dsModels = [
-    {
-        "mgiName": "Ts(17<16>)65Dn (001924)",
-        "commonName": "Ts65Dn (001924)",
-        "type": "Radiation induced translocation",
-        "background": "B6EiC3Sn",
-        "genes": "101",
-        "availability": "https://www.jax.org/strain/001924",
-        "rrid": "RRID:IMSR_JAX:001924"
-    },
-    {
-        "mgiName": "Ts(17<16>)65Dn (005252)",
-        "commonName": "Ts65Dn (005252)",
-        "type": "Congenic/Radiation induced",
-        "background": "B6EiC3Sn.BLiA",
-        "genes": "101",
-        "availability": "https://www.jax.org/strain/005252",
-        "rrid": "RRID:IMSR_JAX:005252"
-    },
-    {
-        "mgiName": "Ts(1716)66Yah",
-        "commonName": "Ts66Yah",
-        "type": "CRISPR/Cas9 derivative of Ts65Dn",
-        "background": "B6C3B",
-        "genes": "101",
-        "availability": "https://www.jax.org/strain/036600",
-        "rrid": "RRID:IMSR_JAX:036600"
-    },
-    {
-        "mgiName": "Ts(16C-tel)1Cje",
-        "commonName": "Ts1Cje",
-        "type": "Translocation",
-        "background": "B6EiC3Sn",
-        "genes": "64",
-        "availability": "https://www.jax.org/strain/004861",
-        "rrid": "RRID:IMSR_JAX:004861"
-    },
-    {
-        "mgiName": "Rb(12.Ts171665Dn)2Cje",
-        "commonName": "Ts2Cje",
-        "type": "Spontaneous Robertsonian translocation",
-        "background": "B6EiC3Sn",
-        "genes": "101",
-        "availability": "https://www.jax.org/strain/004850",
-        "rrid": "RRID:IMSR_JAX:004850"
-    },
-    {
-        "mgiName": "Ts(16)1Yey",
-        "commonName": "Ts1Yey / Dp(16Lipi-Zbtb21)1Yey",
-        "type": "Segmental Trisomy (Cre-lox)",
-        "background": "B6;129P2 / C57BL/6J",
-        "genes": "114",
-        "availability": "https://www.jax.org/strain/013530",
-        "rrid": "RRID:IMSR_JAX:013530"
-    },
-    {
-        "mgiName": "Ts(17)1Yey",
-        "commonName": "Ts1Yey / Dp(17Abcg1-Rrp1b)1Yey",
-        "type": "Segmental Trisomy (Cre-lox)",
-        "background": "B6;129P2 / C57BL/6J",
-        "genes": "19",
-        "availability": "https://www.jax.org/strain/013529",
-        "rrid": "RRID:IMSR_JAX:013529"
-    },
-    {
-        "mgiName": "Ts(10)1Yey",
-        "commonName": "Ts10Yey / Dp(10Prmt2-Adarb1)1Yey",
-        "type": "Segmental Trisomy (Cre-lox)",
-        "background": "B6;129P2 / C57BL/6J",
-        "genes": "41",
-        "availability": "https://www.jax.org/strain/013531",
-        "rrid": "RRID:IMSR_JAX:013531"
-    },
-    {
-        "mgiName": "Tc(HSA21,CAG-EGFP)1Yakaz",
-        "commonName": "TcMAC21",
-        "type": "Transchromosomic",
-        "background": "B6D2",
-        "genes": "581",
-        "availability": "https://www.jax.org/strain/035561",
-        "rrid": "RRID:IMSR_JAX:035561"
-    },
-    {
-        "mgiName": "Del(16Lipi-Zbtb21)1Yey",
-        "commonName": "Ms1Yey",
-        "type": "Deletion",
-        "background": "B6;129P2",
-        "genes": "114",
-        "availability": "https://www.jax.org/strain/013532",
-        "rrid": "RRID:IMSR_JAX:013532"
-    }
-    // Note: To keep this brief, I included the most common ones. 
-    // You can add the remaining 34 by following this pattern!
+  {
+    "mgiName": "Ts(17<16>)65Dn (001924)",
+    "commonName": "Ts65Dn (001924)",
+    "type": "Radiation induced translocation",
+    "background": "B6EiC3Sn",
+    "genes": "101",
+    "availability": "https://www.jax.org/strain/001924",
+    "rrid": "RRID:IMSR_JAX:001924",
+    "description": "Trisomy for a marker chromosome containing Mmu16 segment translocated to Mmu17."
+  },
+  {
+    "mgiName": "Ts(17<16>)65Dn (005252)",
+    "commonName": "Ts65Dn (005252)",
+    "type": "Congenic",
+    "background": "B6EiC3Sn.BLiA",
+    "genes": "101",
+    "availability": "https://www.jax.org/strain/005252",
+    "rrid": "RRID:IMSR_JAX:005252",
+    "description": "Homozygous for wild-type Pde6b to eliminate retinal degeneration."
+  },
+  {
+    "mgiName": "Ts(1716)66Yah",
+    "commonName": "Ts66Yah",
+    "type": "CRISPR/Cas9 derivative",
+    "background": "B6C3B",
+    "genes": "101",
+    "availability": "https://www.jax.org/strain/036600",
+    "rrid": "RRID:IMSR_JAX:036600",
+    "description": "Removes non-Hsa21 homologous regions from Ts65Dn."
+  },
+  {
+    "mgiName": "Ts(16C-tel)1Cje",
+    "commonName": "Ts1Cje",
+    "type": "Translocation",
+    "background": "B6EiC3Sn",
+    "genes": "64",
+    "availability": "https://www.jax.org/strain/004861",
+    "rrid": "RRID:IMSR_JAX:004861",
+    "description": "Trisomic for Sod1 to Mx1 segment translocated to Mmu12."
+  },
+  {
+    "mgiName": "Rb(12.Ts171665Dn)2Cje",
+    "commonName": "Ts2Cje",
+    "type": "Robertsonian translocation",
+    "background": "B6EiC3Sn",
+    "genes": "101",
+    "availability": "https://www.jax.org/strain/004850",
+    "rrid": "RRID:IMSR_JAX:004850",
+    "description": "Spontaneous fusion between Ts65Dn marker and Mmu12."
+  },
+  {
+    "mgiName": "Tc(HSA21)1TybEmcf",
+    "commonName": "Tc1",
+    "type": "Transchromosomic",
+    "background": "B6129S",
+    "genes": "227*",
+    "availability": "https://www.jax.org/strain/010801",
+    "rrid": "RRID:IMSR_JAX:010801",
+    "description": "Contains freely segregating copy of Hsa21; mosaic."
+  },
+  {
+    "mgiName": "Tc(HSA21,CAG-EGFP)1Yakaz",
+    "commonName": "TcMAC21",
+    "type": "Transchromosomic",
+    "background": "B6D2",
+    "genes": "581",
+    "availability": "https://www.jax.org/strain/035561",
+    "rrid": "RRID:IMSR_JAX:035561",
+    "description": "Contains freely segregating copy of Hsa21 (93% representation)."
+  },
+  {
+    "mgiName": "Dp(16Cbr1-Fam3b)1Rhr",
+    "commonName": "Ts1Rhr",
+    "type": "Duplication",
+    "background": "B6.129S6",
+    "genes": "28",
+    "availability": "https://www.jax.org/strain/005383",
+    "rrid": "RRID:IMSR_JAX:005383",
+    "description": "Duplication of DSCR genes syntenic with distal Hsa21q22."
+  },
+  {
+    "mgiName": "Dp(16Lipi-Zbtb21)1Yey",
+    "commonName": "Dp(16)1Yey",
+    "type": "Duplication",
+    "background": "B6.129S7",
+    "genes": "117",
+    "availability": "https://www.jax.org/strain/013530",
+    "rrid": "RRID:IMSR_JAX:013530",
+    "description": "Large duplication on Mmu16."
+  },
+  {
+    "mgiName": "Dp(16Tiam1-Kcnj6)6Yey",
+    "commonName": "Dp(16)2Yey",
+    "type": "Duplication",
+    "background": "B6;129S7",
+    "genes": "52",
+    "availability": "https://www.jax.org/strain/018852",
+    "rrid": "RRID:IMSR_JAX:018852",
+    "description": "Duplication including Tiam1 and Kcnj6."
+  },
+  {
+    "mgiName": "Dp(10Prmt2-Pdxk)2Yey",
+    "commonName": "Dp(10)1Yey",
+    "type": "Duplication",
+    "background": "B6;129S7",
+    "genes": "37",
+    "availability": "https://www.jax.org/strain/013529",
+    "rrid": "RRID:IMSR_JAX:013529",
+    "description": "Mmu10 duplication syntenic with distal Hsa21q22."
+  },
+  {
+    "mgiName": "Dp(17Abcg1-Rrp1b)3Yey",
+    "commonName": "Dp(17)1Yey",
+    "type": "Duplication",
+    "background": "B6;129S7",
+    "genes": "18",
+    "availability": "https://www.jax.org/strain/013531",
+    "rrid": "RRID:IMSR_JAX:013531",
+    "description": "Mmu17 duplication syntenic with proximal Hsa21q22."
+  },
+  {
+    "mgiName": "Dp(16Tiam1-Il10rb)8Yey",
+    "commonName": "Dp(16)3Yey",
+    "type": "Duplication",
+    "background": "129Sv",
+    "genes": "17",
+    "availability": "https://www.jax.org/strain/024906",
+    "rrid": "RRID:IMSR_JAX:024906",
+    "description": "Small Mmu16 duplication."
+  },
+  {
+    "mgiName": "Dp(16Ifnar1-Kcnj6)10Yey",
+    "commonName": "Dp(16)4Yey",
+    "type": "Duplication",
+    "background": "129Sv",
+    "genes": "35",
+    "availability": "https://www.jax.org/strain/024908",
+    "rrid": "RRID:IMSR_JAX:024908",
+    "description": "Mmu16 duplication including Ifnar1."
+  },
+  {
+    "mgiName": "Dp(17Abcg1-Cbs)1Yah",
+    "commonName": "Ts1Yah",
+    "type": "Duplication",
+    "background": "B6;129P2",
+    "genes": "13",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=1987",
+    "rrid": "RRID:IMSR_EM:1987",
+    "description": "Mmu17 duplication syntenic to Hsa21q22."
+  },
+  {
+    "mgiName": "Dp(16Hspa13-App)2Yah",
+    "commonName": "Ts2Yah",
+    "type": "Duplication",
+    "background": "B6;129P2",
+    "genes": "19",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=2101",
+    "rrid": "RRID:IMSR_EM:2101",
+    "description": "Mmu16 duplication homologous to Hsa21q11-q21."
+  },
+  {
+    "mgiName": "Dp(16Lipi-Zbtb21)1TybEmcf",
+    "commonName": "Dp1Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "117",
+    "availability": "https://www.jax.org/strain/037183",
+    "rrid": "RRID:IMSR_JAX:037183",
+    "description": "Cre-mediated Mmu16 duplication (Lipi to Zbtb21)."
+  },
+  {
+    "mgiName": "Dp(16Mis18a-Runx1)2TybEmcf",
+    "commonName": "Dp2Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "32",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10558",
+    "rrid": "RRID:IMSR_EM:10558",
+    "description": "Mmu16 duplication (Mis18a to Runx1)."
+  },
+  {
+    "mgiName": "Dp(16Mir802-Zbtb21)3TybEmcf",
+    "commonName": "Dp3Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "37",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10559",
+    "rrid": "RRID:IMSR_EM:10559",
+    "description": "Mmu16 duplication (Mir802 to Zbtb21)."
+  },
+  {
+    "mgiName": "Dp(16Mir802-Dscr3)4TybEmcf",
+    "commonName": "Dp4Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "14",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10560",
+    "rrid": "RRID:IMSR_EM:10560",
+    "description": "Mmu16 duplication (Mir802 to Dscr3)."
+  },
+  {
+    "mgiName": "Dp(16Dyrk1a-B3galt5)5TybEmcf",
+    "commonName": "Dp5Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "12",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10561",
+    "rrid": "RRID:IMSR_EM:10561",
+    "description": "Mmu16 duplication (Dyrk1a to B3galt5)."
+  },
+  {
+    "mgiName": "Dp(16Igsf5-Zbtb21)6TybEmcf",
+    "commonName": "Dp6Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "11",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10562",
+    "rrid": "RRID:IMSR_EM:10562",
+    "description": "Mmu16 duplication (Igsf5 to Zbtb21)."
+  },
+  {
+    "mgiName": "Dp(16Lipi-Hunk)9TybEmcf",
+    "commonName": "Dp9Tyb",
+    "type": "Duplication",
+    "background": "B6.129P2",
+    "genes": "48",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=10565",
+    "rrid": "RRID:IMSR_EM:10565",
+    "description": "Mmu16 duplication (Lipi to Hunk)."
+  },
+  {
+    "mgiName": "Del(16Cbr1-Fam3b)1Rhr",
+    "commonName": "Ms1Rhr",
+    "type": "Deletion",
+    "background": "B6C3",
+    "genes": "29",
+    "availability": "https://www.jax.org/strain/005654",
+    "rrid": "RRID:IMSR_JAX:005654",
+    "description": "Deletion of genes syntenic with distal Hsa21q22."
+  },
+  {
+    "mgiName": "Del(10Prmt2-Pdxk)4Yey",
+    "commonName": "Df(10)1Yey",
+    "type": "Deletion",
+    "background": "Mixed 129",
+    "genes": "37",
+    "availability": "https://www.jax.org/strain/017439",
+    "rrid": "RRID:IMSR_JAX:017439",
+    "description": "Mmu10 deletion syntenic with distal Hsa21q22."
+  },
+  {
+    "mgiName": "Del(17Abcg1-Rrp1b)5Yey",
+    "commonName": "Df(17)1Yey",
+    "type": "Deletion",
+    "background": "Mixed 129",
+    "genes": "18",
+    "availability": "https://www.jax.org/strain/017440",
+    "rrid": "RRID:IMSR_JAX:017440",
+    "description": "Mmu17 deletion syntenic with proximal Hsa21q22."
+  },
+  {
+    "mgiName": "Del(16Tiam1-Kcnj6)7Yey",
+    "commonName": "Df(16)2Yey",
+    "type": "Deletion",
+    "background": "Mixed 129",
+    "genes": "52",
+    "availability": "https://www.jax.org/strain/018853",
+    "rrid": "RRID:IMSR_JAX:018853",
+    "description": "Mmu16 deletion (Tiam1 to Kcnj6)."
+  },
+  {
+    "mgiName": "Del(16Tiam1-Il10rb)9Yey",
+    "commonName": "Df(16)3Yey",
+    "type": "Deletion",
+    "background": "Mixed 129",
+    "genes": "17",
+    "availability": "https://www.jax.org/strain/024907",
+    "rrid": "RRID:IMSR_JAX:024907",
+    "description": "Small Mmu16 deletion (Tiam1 to Il10rb)."
+  },
+  {
+    "mgiName": "Del(16Ifnar1-Kcnj6)11Yey",
+    "commonName": "Df(16)4Yey",
+    "type": "Deletion",
+    "background": "Mixed 129",
+    "genes": "35",
+    "availability": "https://www.jax.org/strain/024908",
+    "rrid": "RRID:IMSR_JAX:024908",
+    "description": "Mmu16 deletion including Ifnar1."
+  },
+  {
+    "mgiName": "Del(16Setd4-Kcnj6)12Yey",
+    "commonName": "Df(16)5Yey",
+    "type": "Deletion",
+    "background": "B6;129S7",
+    "genes": "15",
+    "availability": "https://www.jax.org/strain/028289",
+    "rrid": "RRID:IMSR_JAX:028289",
+    "description": "Mmu16 deletion (Setd4 to Kcnj6)."
+  },
+  {
+    "mgiName": "Del(16Kcnj15-Mx2)13Yey",
+    "commonName": "Df(16)6Yey",
+    "type": "Deletion",
+    "background": "B6.129S7",
+    "genes": "16",
+    "availability": "https://www.jax.org/strain/028292",
+    "rrid": "RRID:IMSR_JAX:028292",
+    "description": "Mmu16 deletion (Kcnj15 to Mx2)."
+  },
+  {
+    "mgiName": "Del(16Dyrk1a-Kcnj6)14Yey",
+    "commonName": "Df(16)7Yey",
+    "type": "Deletion",
+    "background": "B6.129S7",
+    "genes": "2",
+    "availability": "https://www.jax.org/strain/028290",
+    "rrid": "RRID:IMSR_JAX:028290",
+    "description": "Small deletion of Dyrk1a and Kcnj6."
+  },
+  {
+    "mgiName": "Del(16Lipi-Ncam2)8Yey/J",
+    "commonName": "Df(16)8Yey",
+    "type": "Deletion",
+    "background": "129S7",
+    "genes": "15",
+    "availability": "https://www.jax.org/strain/038567",
+    "rrid": "RRID:IMSR_JAX:038567",
+    "description": "Mmu16 deletion (Lipi to Ncam2)."
+  },
+  {
+    "mgiName": "Del(17Scaf8-Pde10a)2Yey/J",
+    "commonName": "Df(17)2Yey",
+    "type": "Deletion",
+    "background": "B6;129S7",
+    "genes": "0",
+    "availability": "https://www.jax.org/strain/038566",
+    "rrid": "RRID:IMSR_JAX:038566",
+    "description": "Deletion of Scaf8 and Pde10a region."
+  },
+  {
+    "mgiName": "Del(10Prmt2-Col6a1)1Yah",
+    "commonName": "Ms1Yah",
+    "type": "Deletion",
+    "background": "B6.129P2",
+    "genes": "12",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=1812",
+    "rrid": "RRID:IMSR_EM:1812",
+    "description": "Mmu16 deletion homologous to Hsa21q22."
+  },
+  {
+    "mgiName": "Del(16Hspa13-App)3Yah",
+    "commonName": "Ms3Yah",
+    "type": "Deletion",
+    "background": "B6;129P2",
+    "genes": "19",
+    "availability": "https://www.infrafrontier.eu/emma/strain-search/straindetails/?q=2100",
+    "rrid": "RRID:IMSR_EM:2100",
+    "description": "Mmu16 deletion homologous to Hsa21q22."
+  }
 ];
